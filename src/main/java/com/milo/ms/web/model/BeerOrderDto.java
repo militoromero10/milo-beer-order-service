1c42231eb8f25501c6458e548667abe1b3ee048b
Camilo Romero <militoromero10@gmail.com>
2021-06-09 11:27:46 -0500

    fixing build error, and adding ssm config

--- src/main/java/com/milo/ms/web/model/BeerOrderDto.java
+++ src/main/java/com/milo/ms/web/model/BeerOrderDto.java
@@ -15,7 +15,7 @@ public class BeerOrderDto extends BaseItem {
     private UUID customerId;
     private String customerRef;
     private List<BeerOrderLineDto> beerOrderLines;
-    private OrderStatusEnum orderStatus;
+    private String orderStatus;
     private String orderStatusCallbackUrl;
 
     public BeerOrderDto() {
@@ -23,7 +23,7 @@ public class BeerOrderDto extends BaseItem {
 
     @Builder
     public BeerOrderDto(UUID id, Integer version, OffsetDateTime createdDate, OffsetDateTime lastModifiedDate, UUID customerId, List<BeerOrderLineDto> beerOrderLines,
-                        OrderStatusEnum orderStatus, String orderStatusCallbackUrl, String customerRef) {
+                        String orderStatus, String orderStatusCallbackUrl, String customerRef) {
         super(id, version, createdDate, lastModifiedDate);
         this.customerId = customerId;
         this.beerOrderLines = beerOrderLines;
--- src/main/java/com/milo/ms/web/model/OrderStatusEnum.java
+++
@@ -1,5 +0,0 @@
-package com.milo.ms.web.model;
-
-public enum OrderStatusEnum {
-    NEW, READY, PICKED_UP
-}
